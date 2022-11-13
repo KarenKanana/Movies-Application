1d032830e9b17235c35194e267a8e10e06e16081
ilyasosman <earlyosman@gmail.com>
2022-11-13 23:55:12 +0300

    Add Toggle like

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -4,6 +4,8 @@ import {
   Add,
   ThumbUpAltOutlined,
   ThumbDownOutlined,
+  ThumbUpIcon,
+  ThumbDownIcon,
 } from "@material-ui/icons";
 import { useState } from "react";
 
@@ -11,6 +13,12 @@ export default function ListItem({ index, img }) {
   const [isHovered, setIsHovered] = useState(false);
   const trailer =
     "https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761";
+
+  const [liked, setLiked] = useState(false)
+
+  const toggleFilledIcon = () => setLiked(!liked)
+
+
   return (
     <div
       className="listItem"
